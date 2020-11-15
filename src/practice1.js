9cded537a6e4dc783df6e713ef4ca9d95a79cd78
yanyuxin <617702281@qq.com>
2020-11-15 11:17:02 +0800

    [yyx] finish practice 2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,14 @@
-const parseData
+const parseData =(input) => {
+    let data = input.data;
+    let column = input.column;
+    let output = [];
+    for (let i = 0; i < data.length; i++) {
+        let object = {};
+        for (let j = 0; j < data[i].length; j++) {
+            object[column[j].name] = data[i][j];
+        }
+        output.push(object);
+    }
+    return output;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,14 @@
-const inject
+const inject =(items,sections) => {
+    let item ={}
+    for (const key in items){
+        item[key] = items[key]
+    }
+    sections.map(iterator =>{
+        const index = iterator.index;
+        const behind = item[index];
+        const nowIndex = items.indexOf(behind)
+        items.splice(nowIndex,0,iterator.content);
+    })
+    return items;
+}
 export { inject };
